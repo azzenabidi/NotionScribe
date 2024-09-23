b2c3f765455ff158ee6478c3ab3178c6e459e979
Azzen Abidi <azzen.abidi@gmail.com>
2024-09-23 15:20:43 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'notion_client'
 require_relative 'notion_template'
 
-client = NotionClient.new("secret_sL7pIrn1mI1EnGqmMBxRJlowjDYJweVKCfshCJm5ASB")
+client = NotionClient.new("NOTION API TOKEN HERE")
 
 NotionTemplate.new(client, "Color") do
   header "Main Header"
